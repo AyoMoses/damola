508b18de8d93dadb4aad79067bbaf61adb9388a4
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2021-02-04 23:50:49 +0100

    slider design fix

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -1,19 +1,19 @@
 // vars
-// 'use strict'
-// var testim = document.getElementById("testim"),
-//   testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
-//   testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
-//   testimLeftArrow = document.getElementById("left-arrow"),
-//   testimRightArrow = document.getElementById("right-arrow"),
-//   testimSpeed = 4500,
-//   currentSlide = 0,
-//   currentActive = 0,
-//   testimTimer,
-//   touchStartPos,
-//   touchEndPos,
-//   touchPosDiff,
-//   ignoreTouch = 30;
-// ;
+'use strict'
+var testim = document.getElementById("testim"),
+  testimDots = Array.prototype.slice.call(document.getElementById("testim-dots").children),
+  testimContent = Array.prototype.slice.call(document.getElementById("testim-content").children),
+  testimLeftArrow = document.getElementById("left-arrow"),
+  testimRightArrow = document.getElementById("right-arrow"),
+  testimSpeed = 4500,
+  currentSlide = 0,
+  currentActive = 0,
+  testimTimer,
+  touchStartPos,
+  touchEndPos,
+  touchPosDiff,
+  ignoreTouch = 30;
+;
 
 window.onload = function () {
 
