63eeb2a5bc5b38197997f90ceffcc7a1e222d41f
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2021-02-06 22:25:04 +0100

    UX fix

--- pages/case-one/case-one.js
+++ pages/case-one/case-one.js
@@ -14,3 +14,13 @@ checkbox.addEventListener('click', function () {
     document.addEventListener('click', listener);
   }
 });
+
+
+// SCROLL
+var contactBtn = document.querySelector('.contact-btn');
+
+var contactEl = document.querySelector('.contact-section');
+
+contactBtn.addEventListener('click', function () {
+  contactEl.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+});
