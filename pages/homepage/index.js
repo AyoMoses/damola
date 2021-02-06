355430ff51a5a67c19ba55a2f7f1ca7f0d4e79a2
odukoyaayodeji@gmail.com <odukoyaayodeji@gmail.com>
2021-02-06 22:04:02 +0100

    UX fix

--- pages/homepage/index.js
+++ pages/homepage/index.js
@@ -126,3 +126,23 @@ checkbox.addEventListener('click', function () {
     document.addEventListener('click', listener);
   }
 });
+
+
+// SCROLL TO VIEW
+var downBtn = document.querySelector('.scroll-down-btn');
+var workExpBtn = document.querySelector('.work-btn');
+var contactBtn = document.querySelector('.contact-btn');
+
+var aboutEl = document.querySelector('.about-section');
+let workEl = document.querySelector('.work-section');
+var contactEl = document.querySelector('.contact-section');
+
+downBtn.addEventListener('click', function () {
+  aboutEl.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
+});
+workExpBtn.addEventListener('click', function () {
+  workEl.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+});
+contactBtn.addEventListener('click', function () {
+  contactEl.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+});
